a27519a74004c1bff9879d9f5b3711877e929b15
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-20 14:49:56 +0200

    child component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AddChild from "./components/addChild/AddChild";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/addchild" element={<AddChild />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
-
-
---
+++ src/components/addChild/AddChild.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import boy from "./../../images/boy.png";
+import girl from "./../../images/girl.png";
+import "./AddChild.css";
+
+function AddChild() {
+  const [childName, setChildName] = useState("");
+  const [gender, setGender] = useState("");
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+
+  useEffect(() => {
+    if (height > 0 && weight > 0) {
+      setBmi(Math.round(weight / (height / 100) ** 2));
+    }
+  }, [height, weight]);
+
+  function validateChildName(e) {
+    setChildName(e.target.value);
+  }
+
+  function handleGender(e) {
+    setGender(e.target.value);
+  }
+
+  function handleWeight(e) {
+    setWeight(Math.abs(e.target.value));
+  }
+
+  function handleHeight(e) {
+    setHeight(Math.abs(e.target.value));
+  }
+
+  return (
+    <div className="child-card">
+      <form>
+        <div className="child-container">
+          <img
+            className="child-avatar"
+            src={gender === "male" ? boy : girl}
+            alt="Child"
+          />
+          <input
+            type="text"
+            placeholder="Imię"
+            value={childName}
+            onChange={validateChildName}
+          />
+        </div>
+        <div className="child-layout">
+          <label htmlFor="birthDay">Data urodzenia</label>
+          <input type="date" id="birthDay" />
+        </div>
+        <div className="child-layout">
+          <label htmlFor="weight">Waga</label>
+          <input
+            type="number"
+            id="weight"
+            value={weight}
+            onChange={handleWeight}
+          />
+        </div>
+        <div className="child-layout">
+          <label htmlFor="height">Wzrost</label>
+          <input
+            type="number"
+            id="height"
+            value={height}
+            onChange={handleHeight}
+          />
+        </div>
+        <div className="child-layout">
+          <label htmlFor="gender">Płeć</label>
+          <div className="child-layout">
+            <div>
+              <label htmlFor="female">Kobieta</label>
+              <input
+                type="radio"
+                id="female"
+                value="female"
+                name="gender"
+                onChange={handleGender}
+              />
+            </div>
+            <div>
+              <label htmlFor="male">Mężczyzna</label>
+              <input
+                type="radio"
+                id="male"
+                value="male"
+                name="gender"
+                onChange={handleGender}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="child-layout">
+          <label htmlFor="bmi">BMI</label>
+          <input type="text" id="bmi" value={bmi} disabled />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddChild;
